2a518bfcd738936334dc8bf49a1d26154a9e1d31
PADA1-2-05 <pada1-2-05@Erxess-iMac.local>
2025-08-24 14:36:51 +0800

    update

---
+++ ex2.js
@@ -0,0 +1,71 @@
+// const a = "abc";
+// const b = 123;
+// const c = new Array();
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+
+// function myFunction(a, b) {
+//     return a + b;
+// }
+
+// function1 = (a, b) => {
+//     return a + b;
+// }
+
+// let  x = 10;
+// x += 5; 
+// console.log(x);
+
+
+// let cars = ["car1", "car2", "car3"];
+
+// const newCars = cars.map(car => car.toUpperCase())
+
+// console.log(newCars);
+// console.log(cars);
+
+// cars.pop() hamgiin suuliin element ustgah
+// cars.push("car4"); hamgiin suuld element nemeh
+// cars.shift() hamgiin ehnii element ustgah
+// cars.unshift("car0"); hamgiin ehend element nemeh
+// cars[0] = cars[0].toUpperCase();
+// cars[1] = cars[1].toUpperCase();
+// cars[2] = cars[2].toUpperCase();
+
+// function carToUpperCase(carName) {
+//     return carName.toUpperCase();
+// }
+
+// cars = cars.map(car => carToUpperCase(car));
+// console.log(cars);
+
+// const ex = [1, 2, 3];
+// console.log(ex.find((num) => num > 2));
+
+// const a = 3;
+// const b = "3";
+
+// console.log(a == b);
+// console.log(a === b);
+
+// function addNumbers(n1, n2) {
+//     return n1 + n2;
+// }
+
+// console.log(addNumbers(3, 2));
+
+// for(let i = 0; i <= 100; i+=2) {
+//     console.log(i);
+// }
+
+// let a = prompt("enter input");
+// while(a !== "end") {
+//     console.log("a: ", a);
+//     a = prompt("enter input");
+// }
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const oddNumbers = numbers.filter(n => n % 2 !== 0);
+// console.log(oddNumbers);
